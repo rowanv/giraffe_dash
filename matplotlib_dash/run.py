eef5c741344564e44d40b64ecf3a2fc3bf821394
rowanv <rrvspam@gmail.com>
2015-10-09 09:42:05 -0500

    Refactored line chart. Removed chat and timeline.

--- matplotlib_dash/run.py
+++ matplotlib_dash/run.py
@@ -141,6 +141,41 @@ def indicator_panels(panel_colour, panel_icon, panel_text):
     panel_html = Markup(panel_html)
     return(panel_html)
 
+def morris_line():
+    line_graph_html = '''
+    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-bar-chart-o fa-fw"></i> Area Chart Example
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                                        Actions
+                                        <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu pull-right" role="menu">
+                                        <li><a href="#">Action</a>
+                                        </li>
+                                        <li><a href="#">Another action</a>
+                                        </li>
+                                        <li><a href="#">Something else here</a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div id="morris-area-chart"></div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+    '''
+    line_graph_html = Markup(line_graph_html)
+    return line_graph_html
 
 #Dashboard Views
 
@@ -152,7 +187,8 @@ def index(**kwargs):
                     indicator_panels('blue', 'comments', 'New comments!'),
                     indicator_panels('green', 'tasks', ''),
                     indicator_panels('yellow', 'shopping_cart', ''),
-                    indicator_panels('red', 'support', '')}}
+                    indicator_panels('red', 'support', '')},
+                'line_graph_html': morris_line(),}
     print('in context')
     print context['panels_html']
     return render_template('index.html', context=context, **kwargs)
