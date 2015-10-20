302a494acac58f96fddcc4258bd258769b9cba2a
rowanv <rrvspam@gmail.com>
2015-10-20 09:01:17 -0500

    Factored out MySQL queries into own file

--- matplotlib_dash/queries.py
+++ matplotlib_dash/queries.py
@@ -1,3 +1,160 @@
+##############
+# Customers
+##############
+
+query_customers_added_last_month = '''
+select count(*)
+from customer
+group by year(create_date), month(create_date)
+order by year(create_date), month(create_date) desc;
+'''
+
+query_customers_lost_last_month = '''
+select count(*)
+from (
+/* inactive customers */
+select *
+from customer
+where (create_date != last_update)
+and (active = FALSE)) inactive_customers
+group by year(last_update), month(last_update)
+limit 1;
+'''
+
+query_active_customers = '''
+select count(*)
+from customer
+where active = True
+'''
+
+query_customers_by_country = '''
+select country, count(*) as number_customers
+from customer_list
+group by country
+order by number_customers desc
+limit 10;'''
+
+query_customers_lost_by_country = '''
+select cl.country, count(*) as number_customers
+from customer_list cl join customer
+on cl.ID = customer.customer_id
+where active = FALSE
+group by country
+order by number_customers desc
+limit 10;
+'''
+
+query_total_customers = '''
+select count(*)
+from customer_list
+'''
+
+##########
+# Sales
+##########
+
+query_sales_last_day = '''
+select sum(amount)
+from payment
+group by date(payment_date)
+order by date(payment_date) desc
+limit 1;
+'''
+query_sales_last_week = '''
+select sum(amount_day)
+from (select sum(amount) as amount_day, date(payment_date)
+from payment
+group by date(payment_date)
+order by date(payment_date) desc
+limit 7) a;
+'''
+query_sales_all_time = '''
+select sum(amount)
+from payment;
+'''
+query_rentals_all_time = '''
+select count(*)
+from rental
+'''
+query_sales_by_movie = '''select *
+from sales_by_film_category;'''
+
+query_payments_by_date_month = '''
+select year(payment_date), month(payment_date), day(payment_date),
+sum(amount)
+from payment
+where month(payment_date) = 7
+group by year(payment_date), month(payment_date), day(payment_date)
+'''
+
+################
+# Inventory
+################
+
+query_avg_length_time_movies_rented = '''
+select AVG(DATEDIFF(return_date, rental_date)) from rental;
+'''
+query_number_rentals_returned_late = '''
+select count(*)
+from (
+select datediff(return_date, rental_date) rental_length
+from rental where datediff(return_date, rental_date) > 7) a;'''
+
+query_films_checked_out = '''
+select count(*)
+from (
+select *
+from rental
+where return_date is null) a;
+'''
+
+query_films_in_inventory = '''
+select count(*)
+from (
+select f.film_id, count(*)
+from film f join inventory i on f.film_id = i.film_id
+group by f.film_id) a;
+'''
+query_movie_inventory_by_category = '''select fc.category_id, name, count(*)
+from film_category fc join category c on fc.category_id = c.category_id
+group by fc.category_id, name'''
+
+query_films_in_inventory_by_store = '''
+select store_id, count(*) from inventory group by store_id;
+'''
+query_top_rented_films = '''
+select f.film_id, f.title, f.release_year, count(*) as number_rentals
+from film f join inventory
+on inventory.film_id = f.film_id
+join rental on rental.inventory_id = inventory.inventory_id
+group by f.film_id, f.title, f.release_year
+order by number_rentals desc
+limit 50;
+'''
+
+############
+# Staff / Employee
+############
+
+query_avg_rental_by_staff = '''
+select avg(total_rented) from (select count(*) as total_rented
+    from rental
+    group by staff_id) a;
+'''
+
+query_rental_by_staff = '''
+select staff_id, year(rental_date), month(rental_date),
+count(*) as total_sales
+from rental
+where year(rental_date) = 2005
+group by staff_id, year(rental_date), month(rental_date);
+'''
+
+
+####
+#Others -- unused
+
+
 query_movies_currently_out = '''select * from rental
 						where return_date is null;'''
 
@@ -5,9 +162,6 @@ query_movies_returned_last_day = '''select *
 from rental
 where date(return_date) = (select date(max(return_date)) from rental);'''
 
-query_movie_inventory_by_category = '''select fc.category_id, name, count(*)
-from film_category fc join category c on fc.category_id = c.category_id
-group by fc.category_id, name'''
 
 query_sales_by_movie_category = '''
 select fc.category_id, c.name, sum(amount)
@@ -45,34 +199,11 @@ select staff_id, year(payment_date), month(payment_date),
     where year(payment_date)=2005 group by staff_id, year(payment_date), month(payment_date) ;
 '''
 
-query_payments_by_date_month = '''
-select year(payment_date), month(payment_date), day(payment_date),
-sum(amount)
-from payment
-where month(payment_date) = 7
-group by year(payment_date), month(payment_date), day(payment_date)
-'''
 
 query_length_time_movies_rented = '''
 select DATEDIFF(return_date, rental_date) from rental;
 '''
 
-query_avg_length_time_movies_rented = '''
-select AVG(DATEDIFF(return_date, rental_date)) from rental;
-'''
-
-query_number_rentals_returned_late = '''select count(*)
-from (
-	select datediff(return_date, rental_date) rental_length
-	from rental where datediff(return_date, rental_date) > 7) a;'''
-
-query_films_checked_out = '''
-select count(*)
-from (
-select *
-from rental
-where return_date is null) a;
-'''
 
 query_customers_rental_overdue = '''
 SELECT CONCAT(customer.last_name, ', ', customer.first_name) AS customer,
@@ -86,28 +217,6 @@ AND rental_date + INTERVAL film.rental_duration DAY < CURRENT_DATE()
 LIMIT 5;
 '''
 
-query_rental_by_staff = '''
-select staff_id, year(rental_date), month(rental_date), count(*)
-from rental
-where year(rental_date) = 2005
-group by staff_id, year(rental_date), month(rental_date);
-'''
-
-query_avg_rental_by_staff = '''
-select avg(total_rented) from (select count(*) as total_rented
-	from rental
-	group by staff_id) a;
-'''
-
-query_top_rented_films = '''
-select f.film_id, f.title, f.release_year, count(*) as number_rentals
-from film f join inventory
-on inventory.film_id = f.film_id
-join rental on rental.inventory_id = inventory.inventory_id
-group by f.film_id, f.title, f.release_year
-order by number_rentals desc
-limit 50;
-'''
 
 
 
--- matplotlib_dash/run.py
+++ matplotlib_dash/run.py
@@ -9,6 +9,7 @@ import brewer2mpl
 from math import ceil
 
 from models import SingleItemResponse, TableItemResponse, Table, IndicatorPanel
+import queries as q
 
 #set static folder
 app = Flask(__name__, static_url_path='/static/dist')
@@ -48,72 +49,36 @@ def read_new_orders():
 # Indicator Panels
 
 def read_total_customers():
-    query_total_customers = '''
-    select count(*)
-    from customer_list
-    '''
+
     response = SingleItemResponse(engine,
-        query_total_customers)
+        q.query_total_customers)
     result = response.fetch_result()
     return result
 
-query_customers_added_last_month = '''
-select count(*)
-from customer
-group by year(create_date), month(create_date)
-order by year(create_date), month(create_date) desc;
-'''
+
 
 def read_customers_added_last_month():
 
     response = SingleItemResponse(engine,
-        query_customers_added_last_month)
+        q.query_customers_added_last_month)
     result = response.fetch_result()
     return result
 
-query_customers_lost_last_month = '''
-select count(*)
-from (
-/* inactive customers */
-select *
-from customer
-where (create_date != last_update)
-and (active = FALSE)) inactive_customers
-group by year(last_update), month(last_update)
-limit 1;
-'''
+
 
 def read_customers_lost_last_month():
-    query_customers_lost_last_month = '''
-    select count(*)
-    from (
-    /* inactive customers */
-    select *
-    from customer
-    where (create_date != last_update)
-    and (active = FALSE)) inactive_customers
-    group by year(last_update), month(last_update)
-    limit 1;
-    '''
+
     response = SingleItemResponse(engine,
-        query_customers_lost_last_month)
+        q.query_customers_lost_last_month)
     result = response.fetch_result()
     return result
 
-query_active_customers = '''
-select count(*)
-from customer
-where active = True
-'''
+
 
 def read_number_active_customers():
-    query_active_customers = '''
-    select count(*)
-    from customer
-    where active = True
-    '''
+
     response = SingleItemResponse(engine,
-        query_active_customers)
+        q.query_active_customers)
     result = response.fetch_result()
     return result
 
@@ -125,28 +90,14 @@ def calc_customer_retention_rate():
     return str(retention_rate) + '%'
 
 def read_customers_by_country():
-    query_customers_by_country = '''
-    select country, count(*) as number_customers
-    from customer_list
-    group by country
-    order by number_customers desc
-    limit 10;'''
-    this_table = Table(engine, query_customers_by_country)
+
+    this_table = Table(engine, q.query_customers_by_country)
     columns = ['Country', 'Number of Customers']
     html_rep = this_table.get_html_rep(columns)
     return html_rep
 
 def read_customers_lost_by_country():
-    query_customers_lost_by_country = '''
-    select cl.country, count(*) as number_customers
-    from customer_list cl join customer
-    on cl.ID = customer.customer_id
-    where active = FALSE
-    group by country
-    order by number_customers desc
-    limit 10;
-    '''
-    this_table = Table(engine, query_customers_lost_by_country)
+    this_table = Table(engine, q.query_customers_lost_by_country)
     columns = ['Country', 'Number of Customers Lost']
     html_rep = this_table.get_html_rep(columns)
     return html_rep
@@ -156,66 +107,26 @@ def read_customers_lost_by_country():
 # Sales
 ##########
 
-query_sales_last_day = '''
-select sum(amount)
-from payment
-group by date(payment_date)
-order by date(payment_date) desc
-limit 1;
-'''
+
 
 def read_sales_last_day():
-    query_sales_last_day = '''
-    select sum(amount)
-    from payment
-    group by date(payment_date)
-    order by date(payment_date) desc
-    limit 1;
-    '''
     response = SingleItemResponse(engine,
-        query_sales_last_day)
+        q.query_sales_last_day)
     result = response.fetch_result()
     return '$ ' + str(result)
-
-query_sales_last_week = '''
-select sum(amount_day)
-from (select sum(amount) as amount_day, date(payment_date)
-from payment
-group by date(payment_date)
-order by date(payment_date) desc
-limit 7) a;
-'''
-
-query_sales_all_time = '''
-select sum(amount)
-from payment;
-'''
-
-query_rentals_all_time = '''
-select count(*)
-from rental
-'''
 
 
 
 def read_sales_by_genre():
-    query_sales_by_movie = '''select *
-    from sales_by_film_category;'''
-    this_table = Table(engine, query_sales_by_movie)
+    this_table = Table(engine, q.query_sales_by_movie)
     columns = ['Category', 'Total Sales']
     html_rep = this_table.get_html_rep(columns)
     return html_rep
 
 def read_sales_last_month_over_time():
-    query_payments_by_date_month = '''
-    select year(payment_date), month(payment_date), day(payment_date),
-    sum(amount)
-    from payment
-    where month(payment_date) = 7
-    group by year(payment_date), month(payment_date), day(payment_date)
-    '''
+
     response = TableItemResponse(engine,
-        query_payments_by_date_month)
+        q.query_payments_by_date_month)
     result = response.fetch_table()
     result = result[['day(payment_date)', 'sum(amount)']]
     result.columns = ['Day', 'Payments']
@@ -236,38 +147,12 @@ def read_sales_last_month_over_time():
 # Inventory
 ################
 
-query_avg_length_time_movies_rented = '''
-select AVG(DATEDIFF(return_date, rental_date)) from rental;
-'''
-query_number_rentals_returned_late = '''
-select count(*)
-from (
-select datediff(return_date, rental_date) rental_length
-from rental where datediff(return_date, rental_date) > 7) a;'''
-
-query_films_checked_out = '''
-select count(*)
-from (
-select *
-from rental
-where return_date is null) a;
-'''
-
-query_films_in_inventory = '''
-select count(*)
-from (
-select f.film_id, count(*)
-from film f join inventory i on f.film_id = i.film_id
-group by f.film_id) a;
-'''
 
 
 def read_films_in_inventory_by_category():
-    query_movie_inventory_by_category = '''select fc.category_id, name, count(*)
-    from film_category fc join category c on fc.category_id = c.category_id
-    group by fc.category_id, name'''
+
     response = TableItemResponse(engine,
-        query_movie_inventory_by_category)
+        q.query_movie_inventory_by_category)
     result = response.fetch_table()
     result = result[['name', 'count(*)']]
     result.columns = ['Category', 'Inventory Count']
@@ -275,27 +160,17 @@ def read_films_in_inventory_by_category():
     return Markup(result_json)
 
 def read_films_in_inventory_by_store():
-    query_films_in_inventory_by_store = '''
-    select store_id, count(*) from inventory group by store_id;
-    '''
+
     response = TableItemResponse(engine,
-        query_films_in_inventory_by_store)
+        q.query_films_in_inventory_by_store)
     result = response.fetch_table()
     result.columns = ['Store ID', 'Number of Films']
     result_json = result.to_json(orient='records')
     return Markup(result_json)
 
 def read_films_in_inventory_most_rented():
-    query_top_rented_films = '''
-    select f.film_id, f.title, f.release_year, count(*) as number_rentals
-    from film f join inventory
-    on inventory.film_id = f.film_id
-    join rental on rental.inventory_id = inventory.inventory_id
-    group by f.film_id, f.title, f.release_year
-    order by number_rentals desc
-    limit 50;
-    '''
-    this_table = Table(engine, query_top_rented_films)
+
+    this_table = Table(engine, q.query_top_rented_films)
     columns = ['Film ID', 'Title', 'Release Year', 'Number of Rentals']
     html_rep = this_table.get_html_rep(columns)
     return html_rep
@@ -307,24 +182,14 @@ def read_films_in_inventory_most_rented():
 # Staff / Employee
 ############
 
-query_avg_rental_by_staff = '''
-select avg(total_rented) from (select count(*) as total_rented
-    from rental
-    group by staff_id) a;
-'''
 
 
 def read_sales_by_employee_over_time():
-    query_rental_by_staff = '''
-    select staff_id, year(rental_date), month(rental_date),
-    count(*) as total_sales
-    from rental
-    where year(rental_date) = 2005
-    group by staff_id, year(rental_date), month(rental_date);
-    '''
+
     response = TableItemResponse(engine,
-        query_rental_by_staff)
+        q.query_rental_by_staff)
     result = response.fetch_table()
+    print(result)
     result = result[['staff_id', 'month(rental_date)', 'total_sales']]
     result = result.pivot(index='month(rental_date)',
         columns='staff_id', values='total_sales')
@@ -335,6 +200,8 @@ def read_sales_by_employee_over_time():
 
 ################
 #Chart Views
+################
+
 def indicator_panels(panel_colour, panel_icon, panel_text, panel_num):
     panel_colour_to_class_mapping = {
         'blue': 'panel-primary',
@@ -431,12 +298,12 @@ def index(**kwargs):
     green_panel = IndicatorPanel(engine, None)
     green_panel.set_values('green', 'comments', 'New Orders!')
     green_panel.panel_num = ('1')
-    red_panel = IndicatorPanel(engine, query_customers_lost_last_month)
+    red_panel = IndicatorPanel(engine, q.query_customers_lost_last_month)
     red_panel.set_values('red', 'support', 'Customers Lost Last Month')
     blue_panel = IndicatorPanel(engine, None)
     blue_panel.set_values('blue', 'tasks', 'Customer Retention Rate')
     blue_panel.panel_num = calc_customer_retention_rate()
-    yellow_panel = IndicatorPanel(engine, query_films_checked_out)
+    yellow_panel = IndicatorPanel(engine, q.query_films_checked_out)
     yellow_panel.set_values('yellow', 'shopping_cart', 'Films Checked Out')
 
     context = {'panels_html': [
@@ -456,11 +323,11 @@ def index(**kwargs):
 
 @app.route('/customers.html')
 def customers(**kwargs):
-    yellow_panel = IndicatorPanel(engine, query_active_customers)
+    yellow_panel = IndicatorPanel(engine, q.query_active_customers)
     yellow_panel.set_values('yellow', 'shopping_cart', 'Active Customers')
-    green_panel = IndicatorPanel(engine, query_customers_added_last_month)
+    green_panel = IndicatorPanel(engine, q.query_customers_added_last_month)
     green_panel.set_values('green', 'tasks', 'Customers Gained Last Month')
-    red_panel = IndicatorPanel(engine, query_customers_lost_last_month)
+    red_panel = IndicatorPanel(engine, q.query_customers_lost_last_month)
     red_panel.set_values('red', 'support', 'Customers Lost Last Month')
     blue_panel = IndicatorPanel(engine, None)
     blue_panel.set_values('blue', 'tasks', 'Customer Retention Rate')
@@ -488,7 +355,7 @@ def employees(**kwargs):
     blue_panel = IndicatorPanel(engine, None)
     blue_panel.set_values('blue', 'tasks', 'Employee Retetention Rate')
     blue_panel.panel_num = ('100%')
-    yellow_panel = IndicatorPanel(engine, query_avg_rental_by_staff)
+    yellow_panel = IndicatorPanel(engine, q.query_avg_rental_by_staff)
     yellow_panel.set_values('yellow', 'shopping_cart',
         'Average Sales per Employee')
     yellow_panel.panel_num = '%0.0f' % yellow_panel.panel_num
@@ -504,17 +371,18 @@ def employees(**kwargs):
 
 @app.route('/inventory.html')
 def inventory(**kwargs):
-    blue_panel = IndicatorPanel(engine, query_films_in_inventory)
+    blue_panel = IndicatorPanel(engine, q.query_films_in_inventory)
     blue_panel.set_values('blue', 'shopping_cart',
         'Film Titles in Inventory')
-    green_panel = IndicatorPanel(engine, query_films_checked_out)
+    green_panel = IndicatorPanel(engine, q.query_films_checked_out)
     green_panel.set_values('green', 'shopping_cart', 'Films Checked Out')
     yellow_panel = IndicatorPanel(engine,
-        query_avg_length_time_movies_rented)
+        q.query_avg_length_time_movies_rented)
     yellow_panel.set_values('yellow', 'tasks',
         'Avg. Days Film Checked Out')
     yellow_panel.panel_num = '%.2f' % green_panel.panel_num
-    red_panel = IndicatorPanel(engine, query_number_rentals_returned_late)
+    red_panel = IndicatorPanel(engine,
+        q.query_number_rentals_returned_late)
     red_panel.set_values('red', 'tasks', 'Rentals Returned Late')
     context = {
         'panels_html': [
@@ -531,10 +399,10 @@ def inventory(**kwargs):
 
 @app.route('/recent_sales.html')
 def recent_sales(**kwargs):
-    blue_panel = IndicatorPanel(engine,query_sales_last_week)
+    blue_panel = IndicatorPanel(engine, q.query_sales_last_week)
     blue_panel.set_values('blue', 'shopping_cart', 'Sales Last Week')
     blue_panel.panel_num = '$' + str(blue_panel.panel_num)
-    yellow_panel = IndicatorPanel(engine, query_sales_last_day)
+    yellow_panel = IndicatorPanel(engine, q.query_sales_last_day)
     yellow_panel.set_values('yellow', 'shopping_cart', 'Sales Last Day')
     context = {
         'panels_html': [
@@ -547,9 +415,9 @@ def recent_sales(**kwargs):
 
 @app.route('/alltime_sales.html')
 def alltime_sales(**kwargs):
-    blue_panel = IndicatorPanel(engine, query_rentals_all_time)
+    blue_panel = IndicatorPanel(engine, q.query_rentals_all_time)
     blue_panel.set_values('blue', 'shopping_cart', 'All-Time Rentals')
-    yellow_panel = IndicatorPanel(engine, query_sales_all_time)
+    yellow_panel = IndicatorPanel(engine, q.query_sales_all_time)
     yellow_panel.set_values('yellow', 'shopping_cart', 'All-Time Sales')
     yellow_panel.panel_num = '$ ' + str(yellow_panel.panel_num)
     context = {
